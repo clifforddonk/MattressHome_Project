3a9cdb7712eeed769cac7e714725d2989817770b
clifforddonk <clifforddonk@gmail.com>
2024-10-23 14:34:21 +0000

    ...

--- src/Routes/Shop.jsx
+++ src/Routes/Shop.jsx
@@ -1,10 +1,30 @@
 import Navbar from "../Components/Navbar";
 import Footer from "../Components/Footer";
+import QuickLinks from "../Components/QuickLinks";
 const Shop = () => {
   return (
     <>
       <Navbar />
-      <main className="mt-[200px] md:m-12 sm:m-0 md:mt-[200px]"></main>
+      <main className="mt-[170px] md:m-12 sm:m-0 md:mt-[150px]">
+        <h1 className="text-center text-2xl font-bold">Choose a filter:</h1>
+        <div className=" flex justify-center">
+          <button className="font-bold font-xl bg-white border-2 border-yellow-300  mx-3 mt-2 py-1 px-4 rounded-xl">
+            Royal Foam
+          </button>
+          <button className="font-bold font-xl bg-white border-2 border-yellow-300 mx-3 mt-2 py-1 px-4 rounded-xl">
+            Latex Foam
+          </button>
+          <button className="font-bold font-xl bg-white border-2 border-yellow-300 mx-3 mt-2 py-1 px-4 rounded-xl">
+            Ash Foam
+          </button>
+          <button className="font-bold font-xl bg-white border-2 border-yellow-300 mx-3 mt-2 py-1 px-4 rounded-xl">
+            Foreign Brand
+          </button>
+        </div>
+      </main>
+      <div>
+        <QuickLinks />
+      </div>
       <footer>
         <Footer />
       </footer>
