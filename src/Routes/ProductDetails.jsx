9f37fc38191fc7f27aeb306de26e469e3a783fa4
clifforddonk <clifforddonk@gmail.com>
2024-11-13 17:14:37 +0000

    Completed ProductDetails Page

--- src/Routes/ProductDetails.jsx
+++ src/Routes/ProductDetails.jsx
@@ -8,6 +8,40 @@ import { TiArrowBack } from "react-icons/ti";
 import { Link } from "react-router-dom";
 import { FaStar } from "react-icons/fa";
 
+const DetailedImages = ({ product }) => {
+  const [error, setError] = useState(null);
+  const [imageUrls, setImageUrls] = useState([]);
+
+  useEffect(() => {
+    const fetchImages = async () => {
+      try {
+        const response = await axiosInstance.get(`/products/${product.id}`);
+        console.log(response);
+        const pictures = response.data.product.pictures.map(
+          (picture) => picture.image_path
+        );
+        setImageUrls(pictures);
+      } catch (error) {
+        setError(error);
+      }
+    };
+    fetchImages();
+  }, [product.id]);
+
+  return (
+    <div className="flex justify-center">
+      {error && <p>Error loading images: {error.message}</p>}
+      {imageUrls.map((url, index) => (
+        <img
+          key={index}
+          className="h-20 w-20 mb-2 shadow-xl p-2 shadow-gray-100 rounded-lg"
+          src={`${import.meta.env.VITE_APP_API_URL}/${url}`}
+          alt={product.name}
+        />
+      ))}
+    </div>
+  );
+};
 const Details = ({ product }) => {
   const imageUrl = `${import.meta.env.VITE_APP_API_URL}/${
     product.pictures[0].image_path
@@ -58,21 +92,18 @@ const Details = ({ product }) => {
         );
     }
   };
+
   return (
     <div className="bg-blue-200  p-8 md:p-14 rounded-xl pt-22 ">
       <div className=" gap-5 w-400 h-300 bg-white grid grid-cols-1 md:grid-cols-2 shadow-xl shadow-gray-400 rounded-xl">
-        <div className="full h-200 overflow-hidden  p-4">
+        <div className="full h-300 overflow-hidden  p-4">
           <img src={imageUrl} alt={product.name} className="p-6" />
-          <div className="mt-4 flex justify-center overflow-hidden">
-            <img
-              className="h-20 w-20 mb-2 shadow-xl p-2 shadow-gray-100 rounded-lg"
-              src={imageUrl}
-              alt={product.name}
-            />
+          <div className="mt-4 overflow-hidden flex-wrap">
+            <DetailedImages product={product} />
           </div>
         </div>
 
-        <div className="w-full h-200 p-4 md:p-10 ">
+        <div className="w-full h-300 p-4 md:p-10 overflow-y-auto">
           <h1 className="font-bold text-3xl">{product.name}</h1>
           <div className=" flex justify-between">
             <h2 className="font-bold text-2xl mt-4">
